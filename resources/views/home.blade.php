1dfc84b7935b7aac8de54008ebb4116d8eda3cd9
elokqi <elokqn2010@gmail.com>
2022-08-08 12:51:06 +0700

    namabahi e

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,7 @@
 
    <ul class="navbar-nav">
      <li class="nav-item active">
-       <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+       <a class="nav-link" href="#">Homeeeeeeeee <span class="sr-only">(current)</span></a>
      </li>
      <li class="nav-item">
        <a class="nav-link" href="#">About Us</a>
